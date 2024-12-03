16be29675a37be8619e5ba6be0b6632f0a124567
Subham-0 <dashsubham2002@gmail.com>
2024-12-03 08:28:14 +0530

    Naive Approach

---
+++ Array/LongestSubarraywithgivenSumK_Positives/BruteforceApproach/Solution.java
@@ -0,0 +1,32 @@
+package Array.LongestSubarraywithgivenSumK_Positives.BruteforceApproach;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static int getLongestSubarray(int arr[] ,long S){
+        int len = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                long sum = 0;
+                for (int k = i; k <= j; k++) {
+                   sum+=arr[k]; 
+                }
+                if(sum == S){
+                    len = Math.max(len, j-i+1);
+                }
+            }
+        }
+        return len;
+    }
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int num = getLongestSubarray(arr,6);
+        System.out.println(num);
+        sc.close();   
+    }    
+}
